81382cac62b1789b6c7920897bb42d1a69e8406f
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-23 00:49:52 +0900

    ✨ FEAT : 추가 좌측하단 FIXED작성버튼

--- application/src/App.js
+++ application/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Introduc from "./components/Introduc";
+import FixedModal from "./components/FixedModal";
 // import Contact from "./components/Contact";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         아직 개발 중인 영역입니다 🚧
       </p>
       <img src="https://item.kakaocdn.net/do/a56e6ddd117688df80be93b1154a9856f43ad912ad8dd55b04db6a64cddaf76d"></img>
+      <FixedModal />
     </div>
   );
 }
