76ef7a5e6dc32800b07d0587b5b086b72f0d3d59
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-25 22:19:58 +0900

    💄 Imsi 컴포넌트

---
+++ application/src/components/Imsi.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function imsi() {
+  return (
+    <div>
+      <p style={{ fontSize: '20px', textAlign: 'center', marginTop: '30px' }}>
+        아직 개발 중인 영역입니다 🚧
+      </p>
+      <img src="https://item.kakaocdn.net/do/a56e6ddd117688df80be93b1154a9856f43ad912ad8dd55b04db6a64cddaf76d"></img>
+    </div>
+  );
+}
