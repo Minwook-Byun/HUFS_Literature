ed2fc07d0ccf3c696d2c2f87b525e3a4abfbf643
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-25 22:17:54 +0900

    ✨ 추가 : 임시 공사중 컴포넌트화

--- application/src/App.js
+++ application/src/App.js
@@ -4,20 +4,16 @@ import Introduc from './components/Introduc';
 import FixedModal from './components/FixedModal';
 import ImageSlider from './components/ImageSlider';
 import { SliderData } from './components/SliderData';
-import Upload from './components/Upload';
 import Contact from './components/Contact';
+import Imsi from './components/Imsi';
 
 function App() {
   return (
     <div>
       <Header />
       <Introduc />
-      <p style={{ fontSize: '20px', textAlign: 'center', marginTop: '30px' }}>
-        아직 개발 중인 영역입니다 🚧
-      </p>
-      <img src="https://item.kakaocdn.net/do/a56e6ddd117688df80be93b1154a9856f43ad912ad8dd55b04db6a64cddaf76d"></img>
       <ImageSlider slides={SliderData} />
-      <Upload />
+      <Imsi />
       <FixedModal />
     </div>
   );
