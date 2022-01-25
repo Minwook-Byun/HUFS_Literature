bb39bd174c297af0483b72c3ae18fe4a15d9f837
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-01-25 10:16:13 +0900

    🎉 시작:  사진 업로드 기능 프로젝트

--- application/src/App.js
+++ application/src/App.js
@@ -4,6 +4,7 @@ import Introduc from './components/Introduc';
 import FixedModal from './components/FixedModal';
 import ImageSlider from './components/ImageSlider';
 import { SliderData } from './components/SliderData';
+import Upload from './components/Upload';
 import Contact from './components/Contact';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       </p>
       <img src="https://item.kakaocdn.net/do/a56e6ddd117688df80be93b1154a9856f43ad912ad8dd55b04db6a64cddaf76d"></img>
       <ImageSlider slides={SliderData} />
+      <Upload />
       <FixedModal />
     </div>
   );
---
+++ application/src/components/Upload.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UploadForm = () => {
+  return (
+    <div>
+      <input name="fileUpload" type="file" />
+    </div>
+  );
+};
+
+export default UploadForm;
